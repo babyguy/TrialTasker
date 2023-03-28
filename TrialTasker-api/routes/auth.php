9fdb907d2c069766bf722b08a0f57c8bd213e04c
babyguy <mangatomax@gmail.com>
2023-03-28 14:30:30 -0500

    cors prueba->3

--- TrialTasker-api/routes/auth.php
+++ TrialTasker-api/routes/auth.php
@@ -9,29 +9,29 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest')
+                ->middleware(['guest','cors'])
                 ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest')
+                ->middleware(['guest','cors'])
                 ->name('login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest')
+                 ->middleware(['guest','cors'])
                 ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest')
+                 ->middleware(['guest','cors'])
                 ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth', 'signed', 'throttle:6,1','cors'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth', 'throttle:6,1','cors'])
                 ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware(['auth','cors'])
                 ->name('logout');
--- TrialTasker-vue/src/axios.js
+++ TrialTasker-vue/src/axios.js
@@ -3,4 +3,5 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =  "http://localhost:8000/" ;
+// axios.defaults.baseURL =  "http://localhost:8000/" ;
+axios.defaults.baseURL =  "https://trialtasker-production-7f61.up.railway.app/" ;
