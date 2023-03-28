6a8f4e590983d2eb8fbc96c4e4acd7ae851019ea
babyguy <mangatomax@gmail.com>
2023-03-28 16:07:24 -0500

    pruebas de cors->8

--- TrialTasker-vue/src/axios.js
+++ TrialTasker-vue/src/axios.js
@@ -4,4 +4,4 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 // axios.defaults.baseURL =  "http://localhost:8000/" ;
-axios.defaults.baseURL =  "https://trialtasker-production-7f61.up.railway.app/" ;
+axios.defaults.baseURL =  "https://trialtasker.up.railway.app/" ;
