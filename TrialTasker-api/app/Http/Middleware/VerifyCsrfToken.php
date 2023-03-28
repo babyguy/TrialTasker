f7970c158f37e851dfd494c6a9fdfe6c513712fe
babyguy <mangatomax@gmail.com>
2023-03-28 14:57:45 -0500

    cors prueba->5

--- TrialTasker-api/app/Http/Middleware/VerifyCsrfToken.php
+++ TrialTasker-api/app/Http/Middleware/VerifyCsrfToken.php
@@ -3,15 +3,18 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array<int, string>
-     */
-    protected $except = [
-        //
-    ];
+  
+    public function handle($request,\Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin','*');
+        $response->header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
+
+        return $response;
+    }
+
 }
