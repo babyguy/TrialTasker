5c2b90cf9cc9a5a257c6c43bfe03683c33a58847
babyguy <mangatomax@gmail.com>
2023-03-28 14:38:32 -0500

    cors prueba->4

--- TrialTasker-api/app/Http/Middleware/Cors.php
+++ TrialTasker-api/app/Http/Middleware/Cors.php
@@ -12,10 +12,10 @@ class Cors
     {
       return $next($request)
          //Url a la que se le dará acceso en las peticiones
-        ->header("Access-Control-Allow-Origin", "http://localhost:3000/")
+        ->header('Access-Control-Allow-Origin', 'http://localhost')
         //Métodos que a los que se da acceso
-        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         //Headers de la petición
-        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization'); 
     }
 }
